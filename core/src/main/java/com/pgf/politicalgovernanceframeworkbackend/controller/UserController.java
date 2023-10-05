6ad0419b722f1b00bfe9154d6c18c39f6a9bc273
owieckowicz <owieckowicz@trans.eu>
2023-10-05 22:05:58 +0200

    code clean

--- core/src/main/java/com/pgf/politicalgovernanceframeworkbackend/controller/UserController.java
+++ core/src/main/java/com/pgf/politicalgovernanceframeworkbackend/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.pgf.politicalgovernanceframeworkbackend.controller;
 
 import com.pgf.politicalgovernanceframeworkbackend.converter.UserToProfileConverter;
-import com.pgf.politicalgovernanceframeworkbackend.dto.AddressDto;
 import com.pgf.politicalgovernanceframeworkbackend.fto.ProfileFto;
-import com.pgf.politicalgovernanceframeworkbackend.dto.UserDto;
 import com.pgf.politicalgovernanceframeworkbackend.service.UserService;
 import java.security.Principal;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,20 +20,6 @@ class UserController {
     @GetMapping
     ProfileFto getUserProfile(Principal principal) {
         return converter.userDtoToProfileFto(userService.findById(principal.getName()));
-        // TODO: zrobic findById(principal.name)
-        // TODO: od nowa eksportowac ustawienia keycloaka zeby uzywalo custom formularza itp. Poprawic tax id generator, oczyscic projekt pod wzgledem spi
-//        return ProfileFto.builder()
-//            .firstName("Oskar")
-//            .lastName("Więckowicz")
-//            .taxId("1982371")
-//            .address(AddressDto.builder()
-//                .country("Poland")
-//                .city("Kielce")
-//                .postalCode("50-123")
-//                .street("Sliczna 44/12")
-//                .build())
-//            .phone("123123123")
-//            .mail("oskar123@gmail.com").build();
     }
 }
 
