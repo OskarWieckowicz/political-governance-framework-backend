ae6d0068fb37c567e90fa39cfa138fa977e236c4
owieckowicz <owieckowicz@trans.eu>
2023-11-20 22:40:15 +0100

    db password

--- core/src/main/java/com/pgf/politicalgovernanceframeworkbackend/PoliticalGovernanceFrameworkBackendApplication.java
+++ core/src/main/java/com/pgf/politicalgovernanceframeworkbackend/PoliticalGovernanceFrameworkBackendApplication.java
@@ -1,9 +1,6 @@
 package com.pgf.politicalgovernanceframeworkbackend;
 
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,19 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @Slf4j
-public class PoliticalGovernanceFrameworkBackendApplication implements CommandLineRunner {
-
-	@Value("${spring.datasource.password}")
-	private String haslo;
-	public static void main(String[] args) {
-		SpringApplication.run(PoliticalGovernanceFrameworkBackendApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// Log the password when the application starts
-		System.out.println("------------------haslo---------------------");
-		System.out.println(haslo);
-	}
-
+public class PoliticalGovernanceFrameworkBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PoliticalGovernanceFrameworkBackendApplication.class, args);
+    }
 }
