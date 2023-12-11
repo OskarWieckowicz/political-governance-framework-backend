8a35c9db5b45120d141506ea4ae6f06a52dccf34
owieckowicz <owieckowicz@trans.eu>
2023-12-11 22:28:36 +0100

    tests fix

--- core/src/test/java/com/pgf/politicalgovernanceframeworkbackend/PoliticalGovernanceFrameworkBackendApplicationTests.java
+++ core/src/test/java/com/pgf/politicalgovernanceframeworkbackend/PoliticalGovernanceFrameworkBackendApplicationTests.java
@@ -2,12 +2,14 @@ package com.pgf.politicalgovernanceframeworkbackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PoliticalGovernanceFrameworkBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- core/src/test/java/com/pgf/politicalgovernanceframeworkbackend/service/impl/CryptoPriceClientServiceImplTests.java
+++ core/src/test/java/com/pgf/politicalgovernanceframeworkbackend/service/impl/CryptoPriceClientServiceImplTests.java
@@ -1,6 +1,7 @@
 package com.pgf.politicalgovernanceframeworkbackend.service.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
@@ -10,6 +11,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,26 +26,43 @@ class CryptoPriceClientServiceImplTests {
     @InjectMocks
     private CryptoPriceClientServiceImpl service;
 
-
     @Test
     void shouldSuccessfullyGetEthUsdPrice() {
-        Double expectedRate = 1000.0;
-        given(restTemplate.getForObject(anyString(), eq(Double.class))).willReturn(expectedRate);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-CoinAPI-Key", "your-api-key");
+
+        ResponseEntity<String> responseEntity = new ResponseEntity<>("{\"rate\":1000.0}", HttpStatus.OK);
+
+        given(restTemplate.exchange(
+            anyString(),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(String.class)))
+            .willReturn(responseEntity);
 
         Double result = service.getEthUsdPrice();
 
-        assertThat(result).isEqualTo(expectedRate);
-
+        assertThat(result).isEqualTo(1000.0);
     }
 
     @Test
     void shouldSuccessfullyGetEthPlnPrice() {
-        Double expectedRate = 1000.0;
-        given(restTemplate.getForObject(anyString(), eq(Double.class))).willReturn(expectedRate);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("X-CoinAPI-Key", "your-api-key");
+
+        ResponseEntity<String> responseEntity = new ResponseEntity<>("{\"rate\":1000.0}", HttpStatus.OK);
+
+        given(restTemplate.exchange(
+            anyString(),
+            eq(HttpMethod.GET),
+            any(HttpEntity.class),
+            eq(String.class)))
+            .willReturn(responseEntity);
 
         Double result = service.getEthPlnPrice();
 
-        assertThat(result).isEqualTo(expectedRate);
-
+        assertThat(result).isEqualTo(1000.0);
     }
 }
